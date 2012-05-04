c88b93530be0b7dd2fc229d389bca5d3cfb8aabb
Sven Herzberg <herzberg.sven@googlemail.com>
2012-05-04 21:50:40 +0200

    make the example a bit easier to use
    * example/.gitignore,
    * example/.npmignore: silence
    * example/config.json.example: example config file
    * example/server.js: read the configuration from "config.json"

--- example/server.js
+++ example/server.js
@@ -1,54 +1,75 @@
 #!/usr/bin/env node
 
 var ayah = require('../index');
+var fs = require('fs');
 var http = require('http');
 var querystring = require('querystring');
 
-// get your own at http://www.areyouahuman.com/
-// FIXME: add a command line prompt here for people to get started
-ayah.configure(XXXXX, XXXXX);
+var config;
 
-http.createServer(function (request, response) {
-		if (request.url !== "/") {
-			response.setHeader('Content-Type', 'text/plain');
-			response.write("404");
-			return response.end();
-		}
+try {
+	config = fs.readFileSync('config.json'); // {"publisherKey": "abc…", "scoringKey": "0123…"}
+	config = JSON.parse(config);
+} catch (e) {
+	console.log("please create config.json from config.json.example: " + e);
+	process.exit(1);
+}
+if (config.publisherKey.indexOf("get your") === 0) {
+	console.log(config.publisherKey);
+	console.log(config.scoringKey);
+	process.exit(1);
+}
 
-		var buf = '';
-		request.on('data', function (data) {
-			buf += data;
-		});
-		request.on('error', function (error) {
-			console.log(error);
-		});
-		request.on('end', function () {
-			if (buf.length) {
-				buf = querystring.parse(buf);
-				ayah.getScore(buf.session_secret, function (error, result) {
-					if (error) {
-						response.setHeader('Content-Type', 'text/plain');
-						response.statusCode = 500;
-						response.write('' + error);
-					} else {
-						response.setHeader('Content-Type', 'text/html');
-						response.write('<html><body><p>Your call was a success</p><pre>' + JSON.stringify(result) + '</pre>' + ayah.recordConversion() + '</html>');
-					}
-					response.end();
-				});
-			} else {
-				response.setHeader('Content-Type', 'text/html');
-				response.write('<html>');
-				response.write('<body>');
-				response.write('<form method="POST" action="/">');
-				response.write(ayah.getPublisherHTML());
-				response.write('<input type="submit"/>');
-				response.write('</form>');
-				response.write('</body>');
-				response.write('</html>');
+ayah.configure(config.publisherKey, config.scoringKey);
+var server = http.createServer(function (request, response) {
+	console.log("got request for " + request.url);
+	
+	if (request.url !== "/") {
+		response.setHeader('Content-Type', 'text/plain');
+		response.write("404");
+		return response.end();
+	}
+	
+	var buf = '';
+	request.on('data', function (data) {
+		console.log("  got %d bytes…");
+		buf += data;
+	});
+	request.on('error', function (error) {
+		console.log(error);
+	});
+	request.on('end', function () {
+		if (buf.length) {
+			console.log("  collected %d bytes of data", buf.length);
+			buf = querystring.parse(buf);
+			ayah.getScore(buf.session_secret, function (error, result) {
+				if (error) {
+					response.setHeader('Content-Type', 'text/plain');
+					response.statusCode = 500;
+					response.write('' + error);
+				} else {
+					response.setHeader('Content-Type', 'text/html');
+					response.write('<html><body><p>Your call was a success</p><pre>' + JSON.stringify(result) + '</pre>' + ayah.recordConversion() + '</html>');
+				}
 				response.end();
-			}
-		});
-}).listen(8080);
+			});
+		} else {
+			console.log("  without data", buf.length);
+			response.setHeader('Content-Type', 'text/html');
+			response.write('<html>');
+			response.write('<body>');
+			response.write('<form method="POST" action="/">');
+			response.write('<input type="input" value="Hello World!"/>');
+			response.write(ayah.getPublisherHTML());
+			response.write('<input type="submit"/>');
+			response.write('</form>');
+			response.write('</body>');
+			response.write('</html>');
+			response.end();
+		}
+	});
+});
 
-console.log('http://localhost:8080/');
+server.listen(8080);
+console.log('the server is running on http://localhost:8080/');
+
